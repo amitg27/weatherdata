c9be0fa6cd14acd32f2eb1cffb7a2e3cbb3fa608
Amit Gurmukhani <112573512+amitg27@users.noreply.github.com>
2023-12-14 17:32:57 +0530

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -1,9 +1,11 @@
-import pymysql
+import pymysql  # Import the pymysql module to enable MySQL database connections in Python.
 
 def getconnection():
-    s = '127.0.0.1' #Your server(host) name 
-    d = 'projectdata' 
-    u = 'root' #Your login user
-    p = 'jobs1471#' #Your login password
+    s = '127.0.0.1'  # Server address (host) for the database, here it's set to localhost.
+    d = 'projectdata'  # Database name to which you want to connect.
+    u = 'root'  # Username for the database login.
+    p = 'jobs1471#'  # Password for the database login.
+    
+    # Establish and return a connection to the database using the provided credentials.
     conn = pymysql.connect(host=s, user=u, password=p, database=d)
     return conn
