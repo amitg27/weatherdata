3a50455e245cd5e1463ab078416302aa01ca7100
Amit Gurmukhani <112573512+amitg27@users.noreply.github.com>
2023-12-14 17:32:36 +0530

    Update weatherdata.py

--- weatherdata.py
+++ weatherdata.py
@@ -1,36 +1,24 @@
-import json
-import time
-import urllib.request
+import json  # Import the json module to parse JSON data.
+import time  # Import the time module (unused in this code snippet).
+import urllib.request  # Import urllib.request to make HTTP requests.
 
-API='JoZpeCmsptmiRaBXJv59aVz1BVxApjLX'
+API = 'JoZpeCmsptmiRaBXJv59aVz1BVxApjLX'  # AccuWeather API key.
 
 def getLocation(city):
-    search_address="http://dataservice.accuweather.com/locations/v1/cities/search?apikey="+API+"&q="+city+"&details=true"
+    # Construct the URL to search for a city using the AccuWeather API.
+    search_address = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + API + "&q=" + city + "&details=true"
     with urllib.request.urlopen(search_address) as search_address:
-        data=json.loads(search_address.read().decode())
-        loaction_key=data[0]['Key']
-        return(loaction_key)
-
-#city='mumbai'
-#locationkey=getLocation(city)
+        # Open the URL and read the response.
+        data = json.loads(search_address.read().decode())
+        # Extract the location key from the response.
+        location_key = data[0]['Key']
+        return location_key  # Return the location key.
 
 def getforecast(locationkey):
-    daily_forecastURL="http://dataservice.accuweather.com/forecasts/v1/daily/1day/"+locationkey+"?apikey="+API+""
-    #print(daily_forecastURL)
+    # Construct the URL to get the daily forecast using the AccuWeather API and the location key.
+    daily_forecastURL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationkey + "?apikey=" + API
     with urllib.request.urlopen(daily_forecastURL) as daily_forecastURL:
-        data=json.loads(daily_forecastURL.read().decode())
-        #print(data["DailyForecasts"])
-        return(data["DailyForecasts"])
-    #print(data)
-
-# =============================================================================
-#     for key1 in data["DailyForecasts"]:
-#         print("Weather Forecast for "+key1['Date']+"is:")
-#         print("Temperature in F (minimum) is: "+str(key1['Temperature']['Minimum']['Value']))
-#         print("Temperature in F (maximum) is: " + str(key1['Temperature']['Maximum']['Value']))
-#         print("Day Forecast "+str(key1['Day']['IconPhrase']))
-#         print("Day Forecast "+str(key1['Night']['IconPhrase']))
-#         print("------------------------------------------------------------------------")
-# 
-# =============================================================================
-#getforecast(locationkey)
+        # Open the URL and read the response.
+        data = json.loads(daily_forecastURL.read().decode())
+        # Return the 'DailyForecasts' part of the response.
+        return data["DailyForecasts"]
